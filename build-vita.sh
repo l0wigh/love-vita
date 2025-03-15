3de2e9d66ea8cd17be74f5a2e273687e8e9b4e01
L0Wigh <mathiotthomas@gmail.com>
2025-03-15 15:33:23 +0100

    (fix): better build script

--- build-vita.sh
+++ build-vita.sh
@@ -3,7 +3,8 @@ platform/vita/automagic
 
 ./configure --host=arm-vita-eabi  --prefix=$VITASDK/arm-vita-eabi/ --disable-shared --enable-static --with-luaversion=5.1 --disable-library-enet --disable-library-luasocket
 
-make -j 6
+grep -rlZ "lpthread" . | xargs -0 sed -i 's///g'
+make -j $(nproc)
 
 vita-elf-create -s ./src/love ./love.velf
 vita-make-fself ./love.velf ./love.self
