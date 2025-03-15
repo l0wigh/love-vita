cc946c6d2d212d504e5e14942128d06c7dc9293e
L0Wigh <mathiotthomas@gmail.com>
2025-03-15 17:41:25 +0100

    Better script

--- build-vita.sh
+++ build-vita.sh
@@ -3,8 +3,7 @@ platform/vita/automagic
 
 ./configure --host=arm-vita-eabi  --prefix=$VITASDK/arm-vita-eabi/ --disable-shared --enable-static --with-luaversion=5.1 --disable-library-enet --disable-library-luasocket
 
-grep -rlZ "lpthread" . | xargs -0 sed -i 's///g'
-make -j $(nproc)
+make -j $(nproc); grep --exclude="build-vita.sh" -rlZ "lpthread" . | xargs -0 sed -i 's/-lpthread//g'; make -j $(nproc);
 
 vita-elf-create -s ./src/love ./love.velf
 vita-make-fself ./love.velf ./love.self
